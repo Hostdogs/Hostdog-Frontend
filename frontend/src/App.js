651f90375bf991eafac6fd0564d2580989a71edf
Pug <Paphankorn>
2021-04-09 21:02:30 +0700

    set Sarabun font in App.css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,19 @@
-import React from 'react'
+import React from "react";
 
-
-
-import {BrowserRouter as Router,Route} from 'react-router-dom';
-import SignUpPage from './components/SignUp/SignUpPage';
-import HomePage from './components/HomePage/HomePage';
-
-
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import SignUpPage from "./components/SignUp/SignUpPage";
+import HomePage from "./components/HomePage/HomePage";
+import "./App.css";
 
 function App() {
-  
-
   return (
- 
     <Router>
-    <div className="App">
-      
-      <Route exact path="/"  component={HomePage}/>
+      <div className="App">
+        <Route exact path="/" component={HomePage} />
 
-      <Route exact path="/signup"  component={SignUpPage}/>
-
-
-    </div>
+        <Route exact path="/signup" component={SignUpPage} />
+      </div>
     </Router>
-
   );
 }
 

d9eef063a41a741e87b7ffb9d4dafb5b9085efe9
Ponlapat Sanguansirikul <54877995+ppbasleng@users.noreply.github.com>
2021-04-15 19:51:15 +0700

    little update

--- frontend/src/components/HistoryPage/HistoryList.js
+++ frontend/src/components/HistoryPage/HistoryList.js
@@ -3,8 +3,8 @@ import {Container} from 'reactstrap'
 export default function HistoryList() {
     return (
         <div>
-            <Container fluid={true}>
-                <br/><h1>HistoryPage</h1>
+            <Container style={{maxWidth:"60vw"}}>
+                <br/><br/><h1>HistoryPage</h1>
             </Container>
         </div>
     )

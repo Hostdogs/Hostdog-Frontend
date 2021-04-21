7ed2cf6fd3716a8e5b6e31f1e7ac0b7c66918612
Pug <Paphankorn>
2021-04-21 13:03:31 +0700

    added gif

--- frontend/src/components/ProgressPage/ProgressBar.js
+++ frontend/src/components/ProgressPage/ProgressBar.js
@@ -5,46 +5,43 @@ import ServiceDetail from './ServiceDetail'
 const progressLabel = [
   "ถึงเวลาบริการแล้วครับ ย้าฮูว...",
   "ผู้รับฝากกำลังรอรับสุนัขของคุณครับ",
-  "กำลังดูแลสุนัขของคุณครับ",
+  "พวกเรากำลังดูแลสุนัขของคุณครับ",
   "ครบกำหนดแล้วกรุณามารับสุนัขของคุณได้ครับ",
-  "บริการสำเร็จแล้วครับ",
+  "บริการเสร็จสิ้นแล้วครับ",
   "คุณได้ยกเลิกบริการแล้วครับ",
   "",
 ];
 const color = [ "warning", "info", "","success" ,"danger"];
+const gif=['/congrat.gif','/waiting.gif','/onhost.gif','./wavecall.gif','/bye.gif','/sad.gif']
 export default function ProgressBar({onCancel}) {
   const [progressValue, setProgressValue] = useState(0);
 
-  const [colorIndex, setColorIndex] = useState(0);
+  const [colorIndex, setColorIndex] = useState(3);
 
   const [labelIndex, setLabelIndex] = useState(0);
   
-  const [imageSize,setImageSize] =useState({
-      height:100,width:80
-  })
+  const [gifIndex,setGifIndex]=useState(0);
 
   const handleProgress = () => {
     console.log(colorIndex );
     if (progressValue >= 100) {
       setLabelIndex(0);
       setProgressValue(20);
-      setColorIndex(0);
-      setImageSize({height:100,width:80});
+      setColorIndex(3);
+      setGifIndex(0);
+   
     } else {
       setLabelIndex(labelIndex + 1);
-      setColorIndex(colorIndex + 1);
+      setColorIndex(3);
       setProgressValue(progressValue + 20);
-      setImageSize({height:imageSize.height*2,width:imageSize.width*2});
-      if (progressValue === 20) {
-        setColorIndex(0);
-
-      }
+      setGifIndex(gifIndex+1);
     }
   };
   const handleCancel=()=>{
     setProgressValue(100);
     setColorIndex(4);
     setLabelIndex(5);
+    setGifIndex(5);
   }
   useEffect(() => {
     setProgressValue(progressValue + 20);
@@ -57,16 +54,19 @@ export default function ProgressBar({onCancel}) {
       <br />
       <br />
       <Container className="progressbar-container">
+      <h2>{progressLabel[labelIndex]}</h2>
           <Container>
-      <img src={process.env.PUBLIC_URL + '/doge.png'} width={imageSize.width.toString()} height= {imageSize.height.toString()}/>
+          <img src={process.env.PUBLIC_URL + gif[gifIndex]}  width="40%" height="40%" />
+        
       </Container>
-        <h2>{progressLabel[labelIndex]}</h2>
+    
         <Progress color={color[colorIndex]} value={progressValue} />
-        <Button onClick={handleProgress} >Progress</Button>
+      
         
       </Container>
 
     <ServiceDetail onCancel={handleCancel}/>
+    <Button onClick={handleProgress} style={{ position: "fixed", bottom: 0, right: 0 }} >Progress</Button>
     </div>
   );
 }

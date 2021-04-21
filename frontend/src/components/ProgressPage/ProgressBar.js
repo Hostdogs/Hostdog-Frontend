93523c89adb973533f8a76dbcd8ef892944af27c
Pug <Paphankorn>
2021-04-21 13:28:26 +0700

    more responsive มั้ง

--- frontend/src/components/ProgressPage/ProgressBar.js
+++ frontend/src/components/ProgressPage/ProgressBar.js
@@ -1,7 +1,7 @@
 import { Container, Label, Row, Col, Progress, Button } from "reactstrap";
 import "./ProgressPage.css";
-import React, { useState,useEffect } from "react";
-import ServiceDetail from './ServiceDetail'
+import React, { useState, useEffect } from "react";
+import ServiceDetail from "./ServiceDetail";
 const progressLabel = [
   "ถึงเวลาบริการแล้วครับ ย้าฮูว...",
   "ผู้รับฝากกำลังรอรับสุนัขของคุณครับ",
@@ -11,41 +11,47 @@ const progressLabel = [
   "คุณได้ยกเลิกบริการแล้วครับ",
   "",
 ];
-const color = [ "warning", "info", "","success" ,"danger"];
-const gif=['/congrat.gif','/waiting.gif','/onhost.gif','./wavecall.gif','/bye.gif','/sad.gif']
-export default function ProgressBar({onCancel}) {
+const color = ["warning", "info", "", "success", "danger"];
+const gif = [
+  "/congrat.gif",
+  "/waiting.gif",
+  "/onhost.gif",
+  "./wavecall.gif",
+  "/bye.gif",
+  "/sad.gif",
+];
+export default function ProgressBar({ onCancel }) {
   const [progressValue, setProgressValue] = useState(0);
 
   const [colorIndex, setColorIndex] = useState(3);
 
   const [labelIndex, setLabelIndex] = useState(0);
-  
-  const [gifIndex,setGifIndex]=useState(0);
+
+  const [gifIndex, setGifIndex] = useState(0);
 
   const handleProgress = () => {
-    console.log(colorIndex );
+    console.log(colorIndex);
     if (progressValue >= 100) {
       setLabelIndex(0);
       setProgressValue(20);
       setColorIndex(3);
       setGifIndex(0);
-   
     } else {
       setLabelIndex(labelIndex + 1);
       setColorIndex(3);
       setProgressValue(progressValue + 20);
-      setGifIndex(gifIndex+1);
+      setGifIndex(gifIndex + 1);
     }
   };
-  const handleCancel=()=>{
+  const handleCancel = () => {
     setProgressValue(100);
     setColorIndex(4);
     setLabelIndex(5);
     setGifIndex(5);
-  }
+  };
   useEffect(() => {
     setProgressValue(progressValue + 20);
-  },[]);
+  }, []);
 
   return (
     <div>
@@ -54,19 +60,23 @@ export default function ProgressBar({onCancel}) {
       <br />
       <br />
       <Container className="progressbar-container">
-      <h2>{progressLabel[labelIndex]}</h2>
-          <Container>
-          <img src={process.env.PUBLIC_URL + gif[gifIndex]}  width="40%" height="40%" />
-        
-      </Container>
-    
+        <h2>{progressLabel[labelIndex]}</h2>
+        <img
+          src={process.env.PUBLIC_URL + gif[gifIndex]}
+          width="40%"
+          height="40%"
+        />
+
         <Progress color={color[colorIndex]} value={progressValue} />
-      
-        
       </Container>
 
-    <ServiceDetail onCancel={handleCancel}/>
-    <Button onClick={handleProgress} style={{ position: "fixed", bottom: 0, right: 0 }} >Progress</Button>
+      <ServiceDetail onCancel={handleCancel} />
+      <Button
+        onClick={handleProgress}
+        style={{ position: "fixed", bottom: 0, right: 0 }}
+      >
+        Progress
+      </Button>
     </div>
   );
 }
--- frontend/src/components/ProgressPage/ServiceDetail.js
+++ frontend/src/components/ProgressPage/ServiceDetail.js
@@ -1,29 +1,28 @@
+import { Container, Label, Button } from "reactstrap";
+import React, { useState } from "react";
+import { format } from "date-fns";
+import "./ProgressPage.css";
+export default function ServiceDetail({ onCancel }) {
+  const [detail, setDetail] = useState({
+    host: "เพียว",
+    dog: "น้องบาส",
+    duration: "20 ก.พ. 64 - 23 ก.พ. 64",
+  });
+  return (
+    <div>
+      <br />
+      <br />
+      <Container className="detail-container">
+        <h3> รายละเอียดการบริการ</h3>
+        <p>ผู้รับฝาก: {detail.host}</p>
+        <p>สุนัข:{detail.dog}</p>
+        <p>ระยะเวลาที่ฝาก: {detail.duration} </p>
 
-import {
-    Container,
-    Label,
-    Button,
+        <Button onClick={onCancel} color="danger">
+          ยกเลิกบริการ
+        </Button>
 
-  } from "reactstrap";
-  import React, { useState } from "react";
-  import { format } from "date-fns";
-export default function ServiceDetail({onCancel}) {
-    const [detail,setDetail]=useState({
-        host:"เพียว",
-        dog:"น้องบาส",
-        duration:"20 ก.พ. 64 - 23 ก.พ. 64"
-
-    });
-    return (
-        <div>
-            <br/><br/>
-   <Container style={{maxWidth: "50vw",height:"40vh", backgroundColor:"#fff3d0",borderRadius:"10px",paddingTop:"10px"}}>
-            <h3> รายละเอียดการบริการ</h3>
-                <p>ผู้รับฝาก: {detail.host}</p>
-                <p>สุนัข:{detail.dog}</p>
-                <p>ระยะเวลาที่ฝาก: {detail.duration} </p>
-                <Button onClick={onCancel} style={{float:"right"}} color="danger">ยกเลิกบริการ</Button>
-            </Container>
-        </div>
-    )
+      </Container>
+    </div>
+  );
 }

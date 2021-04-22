a9c05707cf20f13ba2dc2e92ec2e8d77058d929a
Pug <Paphankorn>
2021-04-22 12:53:37 +0700

    lift state up to progresspage

--- frontend/src/components/ProgressPage/ProgressBar.js
+++ frontend/src/components/ProgressPage/ProgressBar.js
@@ -1,7 +1,6 @@
 import { Container, Label, Row, Col, Progress, Button } from "reactstrap";
 import "./ProgressPage.css";
-import React, { useState, useEffect } from "react";
-import ServiceDetail from "./ServiceDetail";
+
 const progressLabel = [
   "ถึงเวลาบริการแล้วครับ ย้าฮูว...",
   "ผู้รับฝากกำลังรอรับสุนัขของคุณครับ",
@@ -20,37 +19,9 @@ const gif = [
   "/bye.gif",
   "/sad.gif",
 ];
-export default function ProgressBar({ onCancel }) {
-  const [progressValue, setProgressValue] = useState(0);
+export default function ProgressBar({progressValue,colorIndex,labelIndex,gifIndex,handleProgress}) {
 
-  const [colorIndex, setColorIndex] = useState(3);
 
-  const [labelIndex, setLabelIndex] = useState(0);
-
-  const [gifIndex, setGifIndex] = useState(0);
-
-  const handleProgress = () => {
-    console.log(colorIndex);
-    if (progressValue >= 100) {
-      setLabelIndex(0);
-      setProgressValue(20);
-      setColorIndex(3);
-      setGifIndex(0);
-    } else {
-      setLabelIndex(labelIndex + 1);
-      setColorIndex(3);
-      setProgressValue(progressValue + 20);
-      setGifIndex(gifIndex + 1);
-    }
-  };
-  const handleCancel = () => {
-    setColorIndex(4);
-    setLabelIndex(5);
-    setGifIndex(5);
-  };
-  useEffect(() => {
-    setProgressValue(progressValue + 20);
-  }, []);
 
   return (
     <div>
@@ -69,7 +40,7 @@ export default function ProgressBar({ onCancel }) {
         <Progress color={color[colorIndex]} value={progressValue} />
       </Container>
 
-      <ServiceDetail onCancel={handleCancel} />
+
       <Button
         onClick={handleProgress}
         style={{ position: "fixed", bottom: 0, right: 0 }}
--- frontend/src/components/ProgressPage/ProgressPage.js
+++ frontend/src/components/ProgressPage/ProgressPage.js
@@ -1,15 +1,48 @@
 import NavbarIsAuth from '../Navbar/NavbarIsAuth';
 import SideBar from '../sidebar/SideBar';
 import ProgressBar from './ProgressBar';
+import React, { useState, useEffect } from "react";
+import ServiceDetail from "./ServiceDetail";
 
 export default function ProgressPage() {
+    const [progressValue, setProgressValue] = useState(0);
+
+    const [colorIndex, setColorIndex] = useState(3);
+  
+    const [labelIndex, setLabelIndex] = useState(0);
+  
+    const [gifIndex, setGifIndex] = useState(0);
+  
+    const handleProgress = () => {
+      if (progressValue >= 100 || gifIndex>=5) {
+        setLabelIndex(0);
+        setProgressValue(20);
+        setColorIndex(3);
+        setGifIndex(0);
+      } else {
+        setLabelIndex(labelIndex + 1);
+        setColorIndex(3);
+        setProgressValue(progressValue + 20);
+        setGifIndex(gifIndex + 1);
+      }
+    };
+    const handleCancel = () => {
+      setColorIndex(4);
+      setLabelIndex(5);
+      setGifIndex(5);
+    };
+    useEffect(() => {
+      setProgressValue(progressValue + 20);
+    }, []);
+
     return (
         <div>
 
             <NavbarIsAuth/>
 
-            <ProgressBar/>
+            <ProgressBar progressValue={progressValue} colorIndex={colorIndex} labelIndex={labelIndex} gifIndex={gifIndex} handleProgress={handleProgress}/>
 
+            <ServiceDetail onCancel={handleCancel} />
 
         </div>
     )

4b14d3ffa4ffc5c943f8edebd54f30f6b9cee47d
Ponlapat Sanguansirikul <54877995+ppbasleng@users.noreply.github.com>
2021-04-26 16:17:18 +0700

    merge successful

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import HomePage from "./components/HomePage/HomePage";
 import ServicePage from "./components/ServicePage/ServicePage";
 import ProfilePage from "./components/UserProfile/ProfilePage"
 import HistoryPage from './components/HistoryPage/HistoryPage';
+import ProgressPage from './components/ProgressPage/ProgressPage'
 import NotFoundPage from "./components/Handle/NotFoundPage";
 
 function App() {
@@ -22,6 +23,8 @@ function App() {
           <Route exact path="/profile/:profile_id" component={ProfilePage} />
 
           <Route exact path="/history" component={HistoryPage}/>
+
+          <Route exact path="/progress" component={HistoryPage}/>
 
           <Route component={NotFoundPage} />
         </Switch>

67c9385627c24ad2e597bcf60d449d51904de619
Pug <Paphankorn>
2021-05-02 00:45:56 +0700

    can pay

--- frontend/src/components/Payment/PaymentModal.js
+++ frontend/src/components/Payment/PaymentModal.js
@@ -1,8 +1,11 @@
 
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter,Label } from 'reactstrap';
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import { faClosedCaptioning } from '@fortawesome/free-solid-svg-icons';
-export default function PaymentModal() {
+import axios from "axios";
+import PaymentAPI from './PaymentAPI'
+export default function PaymentModal({service_id,payment_id}) {
+
 const [modal, setModal] = useState(false);
   const [nestedModal, setNestedModal] = useState(false);
   const [closeAll, setCloseAll] = useState(false);
@@ -11,7 +14,7 @@ const [modal, setModal] = useState(false);
   const [customerName,setCustomerName]=useState("บาส บางขุนเทียน")
   const [dogName,setDogName]=useState("น้องบาส หลังอาน")
   const [accountNumber,setAccountNumber]=useState("1234567890");
-  const [totalPrice,setTotalPrice]=useState(30.33)
+  const [totalPrice,setTotalPrice]=useState(null)
 
 
   const toggleNested = () => {
@@ -22,26 +25,42 @@ const [modal, setModal] = useState(false);
     setNestedModal(!nestedModal);
     setCloseAll(true);
   }
+
+  const handlePay=()=>{
+    toggleAll();
+    const data={accept_payment:true}
+    PaymentAPI.pay(service_id,payment_id,data)
+  }
+  const getPayment=async()=>{
+      const response= await PaymentAPI.getPayment(service_id,payment_id)
+      const payment =response.data
+      console.log("payment")
+      console.log(payment)
+      setTotalPrice(payment.pay_total)
+  }
+  const listPayment=async()=>{
+      const response=await PaymentAPI.listPayment(service_id)
+      const listPayment=response.data
+      console.log("listPayment")
+      console.log(listPayment)
+  }
+
+  useEffect(()=>{
+    listPayment();
+
+  },[]);
+  
+
     return (
         <div>
             <Button color="danger" onClick={toggle}>จ่ายเงินกันเถอะ ย้าฮูว</Button>
             <Modal isOpen={modal} toggle={toggle} fade={false} >
         <ModalHeader toggle={toggle}><h2>ชำระเงินค่าบริการ</h2></ModalHeader>
         <ModalBody>
-        <div>
 
-        {/* <h5>ชื่อ </h5>
-        <p style={{textIndent:"20px"}}>{customerName}</p>
-        <h5>สุนัข </h5>
-        <p style={{textIndent:"20px"}}>{dogName}</p>
-         <h5>หมายเลขบัญชี</h5>
-         <p style={{textIndent:"20px"}}>{accountNumber}</p>
-         <h5>ยอดชำระเงินรวม</h5>
-         <p style={{textIndent:"20px"}}> {totalPrice} บาท</p> */}
          <div style={{justifyContent:"space-between",display:"flex"}}>
-                <h5>ชื่อ:</h5>
-                 <Label>{customerName}</Label>
-        
+        <h5>ชื่อ:</h5>
+        <Label>{customerName}</Label>
         </div>
         <div style={{justifyContent:"space-between",display:"flex"}}>
         <h5>สุนัข:</h5>
@@ -55,16 +74,12 @@ const [modal, setModal] = useState(false);
          <h5>ยอดชำระเงินรวม:</h5>
          <Label > {totalPrice} บาท</Label>
          </div>
-
-
-         </div>
-
          <small style={{ color: "red" }}>หมายเหตุ: กรุณาชำระค่าบริการภายใน 1 ชั่วโมง มิฉะนั้นบริการของคุณจะถูกยกเลิก</small>
           <br />
           <Modal isOpen={nestedModal} fade={false} toggle={toggleNested} onClosed={closeAll ? toggle : undefined}>
             <ModalBody>คุณต้องการยืนยันการชำระเงินใช่หรือไม่</ModalBody>
             <ModalFooter>
-              <Button color="success" onClick={toggleAll}>ใช่</Button>{' '}
+              <Button color="success" onClick={handlePay}>ยืนยัน</Button>{' '}
               <Button color="danger" onClick={toggleNested}>ยกเลิก</Button>
             </ModalFooter>
           </Modal>

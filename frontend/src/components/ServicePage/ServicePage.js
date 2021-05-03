1dcde7a4d3235811777f090327aa63d527d2bfda
Pug <Paphankorn>
2021-05-03 20:43:10 +0700

    undone date host picture address

--- frontend/src/components/ServicePage/ServicePage.js
+++ frontend/src/components/ServicePage/ServicePage.js
@@ -18,7 +18,7 @@ export default function ServicePage() {
             <ServiceHost />
           </Col>
           <Col xs="12" sm="12" md="12" lg="9">
-            <ServiceForm mytoken={"ac5ff12742a6890c67a44a844e203d6ed5eee68f"} host_id={3} customer_id={1} />
+            <ServiceForm host_id={3} customer_id={1} />
             <br />
           </Col>
         </Row>

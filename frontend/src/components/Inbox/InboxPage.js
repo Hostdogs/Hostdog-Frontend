71a8b4cfcf6e0d4cccdc72e5edba8e821f9beafc
Ponlapat Sanguansirikul <54877995+ppbasleng@users.noreply.github.com>
2021-05-04 22:43:40 +0700

    update API

--- frontend/src/components/Inbox/InboxPage.js
+++ frontend/src/components/Inbox/InboxPage.js
@@ -12,22 +12,28 @@ export default function InboxPage() {
     useEffect(() => {
         // setpendingData([{ service_id: "1", customer_id: "1", dog_id: "1", main_status: "Pending", }, { service_id: "1", customer_id: "1", dog_id: "1", main_status: "Pending", }])
         if (cookies["mytoken"]) {
-            ServiceAPI.getAllPending(cookies["mytoken"]).then(res => {
-                console.log("Pendingdata:", res.data)
-                setpendingData(res.data)
-                setisLoad(true)
-            })
+            getPending()
+            console.log("interval set")
+            setInterval(()=>getPending(),5000);
         }
 
     }, [cookies])
-  
+
+    const getPending = () => {
+        ServiceAPI.getAllPending(cookies["mytoken"]).then(res => {
+            console.log("Pendingdata:", res.data)
+            setpendingData(res.data)
+            setisLoad(true)
+        })
+    }
+
     return (
         <div style={{ paddingTop: "75px" }}>
             <Container>
                 <h1 className="heading1">รายการคำร้องขอของคุณ :</h1>
             </Container>
             <div style={{ minHeight: "100vh" }}>
-                {isLoad && pendingData.length > 0 ? (<PendingList pendingData={pendingData}  />) : (null)}
+                {isLoad && pendingData.length > 0 ? (<PendingList pendingData={pendingData} />) : (null)}
                 {isLoad && pendingData.length === 0 ? (<BlankPending />) : (null)}
             </div>
 
